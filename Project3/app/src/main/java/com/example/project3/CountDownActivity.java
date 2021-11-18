9b4154a0330bb3131543f83ac6f7eec4888dcd45
isabellaattisano <iattisan@villanova.edu>
2021-11-18 18:48:45 -0500

    Project Setup

---
+++ Project3/app/src/main/java/com/example/project3/CountDownActivity.java
@@ -0,0 +1,14 @@
+package com.example.project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CountDownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count_down);
+    }
+}
