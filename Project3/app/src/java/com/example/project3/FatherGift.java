f0159836f1bc9e760a646983087d2f2396b84ffc
isabellaattisano <85297147+isabellaattisano@users.noreply.github.com>
2021-11-30 13:05:36 -0500

    update manifest

---
+++ Project3/app/src/java/com/example/project3/FatherGift.java
@@ -0,0 +1,14 @@
+package com.example.project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FatherGift extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_father_gift);
+    }
+}
---
+++ Project3/app/src/java/com/example/project3/MomGift.java
@@ -0,0 +1,13 @@
+package com.example.project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MomGift extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mom_gift);
+    }
+}
